5e57b38ec554a44ff132607033e18c5c4dbb633f
Joilson Junior <joilson320@hotmail.com>
2024-07-12 17:13:52 -0300

    realizando novos ajustes no banco Usuario.js para o deploy

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,16 +1,22 @@
 // COMINICAÇÃO DO BANCO COM O RENDER
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "mysql",
-  define: {
-    charsets: "utf8",
-    collate: "utf8_unicode_ci",
-    timeStamp: true,
-  },
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // A porta do MySQL
+    dialect: "mysql",
+    define: {
+      charsets: "utf8",
+      collate: "utf8_unicode_ci",
+      timeStamp: true,
+    },
+    logging: false,
+  }
+);
 
 // TESTANDO A CONEXÃO COM O BANCO
 sequelize.authenticate().then(() => {
