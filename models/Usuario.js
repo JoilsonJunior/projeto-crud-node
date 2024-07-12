56c1d28338b912989b299c70ad5e24f082e7c4a0
Joilson Junior <joilson320@hotmail.com>
2024-07-12 17:47:40 -0300

    Foi realizado uma nova atualização devido alguns erros de codigo

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,56 +1,24 @@
-// COMINICAÇÃO DO BANCO COM O RENDER
-const Sequelize = require("sequelize");
+const db = require("./db");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // A porta do MySQL
-    dialect: "mysql",
-    define: {
-      charsets: "utf8",
-      collate: "utf8_unicode_ci",
-      timeStamp: true,
-    },
-    logging: false,
-  }
-);
+const Usuario = db.sequelize.define("usuario", {
+  id: {
+    type: db.Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
 
-// TESTANDO A CONEXÃO COM O BANCO
-sequelize.authenticate().then(() => {
-  console.log("Conexão com o banco realizada com sucesso");
-}).catch((err) => {
-  console.log("Falha na conexão: " + err);
+  nome: {
+    type: db.Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: db.Sequelize.STRING,
+    allowNull: false,
+    // unique: true, // pra manter o email unico e não acontecer de registrar o mesmo email no banco
+  },
 });
 
-module.exports = { Sequelize, sequelize };
+Usuario.sync({ alter: true }); // alter: true juntamente com unique: true - Verifica diferenças
 
-
-// BANCO LOCAL XAMMP
-
-// const db = require("./db");
-
-// const Usuario = db.sequelize.define("usuario", {
-//   id: {
-//     type: db.Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true,
-//   },
-
-//   nome: {
-//     type: db.Sequelize.STRING,
-//     allowNull: false,
-//   },
-//   email: {
-//     type: db.Sequelize.STRING,
-//     allowNull: false,
-//     // unique: true, // pra manter o email unico e não acontecer de registrar o mesmo email no banco
-//   },
-// });
-
-// Usuario.sync({ alter: true }); // alter: true juntamente com unique: true - Verifica diferenças
-
-// module.exports = Usuario;
+module.exports = Usuario;
--- models/db.js
+++ models/db.js
@@ -1,24 +1,58 @@
+// COMINICAÇÃO DO BANCO COM O RENDER
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("fomulario_crud", "root", "", {
-  host: "127.0.0.1",
-  dialect: "mysql",
-  define: {
-    charsets: "utf8",
-    collate: "utf8_unicode_ci",
-    timeStamp: true,
-  },
-  logging: false,
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // A porta do MySQL
+    dialect: "mysql",
+    define: {
+      charsets: "utf8",
+      collate: "utf8_unicode_ci",
+      timeStamp: true,
+    },
+    logging: false,
+  }
+);
+
+// TESTANDO A CONEXÃO COM O BANCO
+sequelize.authenticate().then(() => {
+  console.log("Conexão com o banco realizada com sucesso");
+}).catch((err) => {
+  console.log("Falha na conexão: " + err);
 });
 
-// TESTANDO A CONECÃO COM O BANCO
+module.exports = { Sequelize, sequelize };
 
-// // .then - significa se deu certo
-// sequelize.authenticate().then(() => {
-// console.log("Conexão com o banco realizada com sucesso");
 
-// // .catch - significa se não de certo
-// }).catch((err) => {
-//     console.log("Falha na conexão" + err);
+
+
+// EXECUÇÃO DO BANCO LOCAL VIA XAMPP
+
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize("fomulario_crud", "root", "", {
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+//   define: {
+//     charsets: "utf8",
+//     collate: "utf8_unicode_ci",
+//     timeStamp: true,
+//   },
+//   logging: false,
 // });
 
-module.exports = { Sequelize, sequelize };
+// // TESTANDO A CONECÃO COM O BANCO
+
+// // // .then - significa se deu certo
+// // sequelize.authenticate().then(() => {
+// // console.log("Conexão com o banco realizada com sucesso");
+
+// // // .catch - significa se não de certo
+// // }).catch((err) => {
+// //     console.log("Falha na conexão" + err);
+// // });
+
+// module.exports = { Sequelize, sequelize }; 
