f052a9d3fa87d52116003ed028ffbd7943bc5438
Joilson Junior <joilson320@hotmail.com>
2024-07-12 16:47:40 -0300

    alteração na Sequelize para comunicação com ambitente render

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,24 +1,50 @@
-const db = require("./db");
+// COMINICAÇÃO DO BANCO COM O RENDER
+const Sequelize = require("sequelize");
 
-const Usuario = db.sequelize.define("usuario", {
-  id: {
-    type: db.Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  define: {
+    charsets: "utf8",
+    collate: "utf8_unicode_ci",
+    timeStamp: true,
   },
-
-  nome: {
-    type: db.Sequelize.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: db.Sequelize.STRING,
-    allowNull: false,
-    unique: true, // pra manter o email unico e não acontecer de registrar o mesmo email no banco
-  },
+  logging: false,
 });
 
-Usuario.sync({ alter: true }); // alter: true juntamente com unique: true - Verifica diferenças
+// TESTANDO A CONEXÃO COM O BANCO
+sequelize.authenticate().then(() => {
+  console.log("Conexão com o banco realizada com sucesso");
+}).catch((err) => {
+  console.log("Falha na conexão: " + err);
+});
 
-module.exports = Usuario;
+module.exports = { Sequelize, sequelize };
+
+
+// BANCO LOCAL XAMMP
+
+// const db = require("./db");
+
+// const Usuario = db.sequelize.define("usuario", {
+//   id: {
+//     type: db.Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+
+//   nome: {
+//     type: db.Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   email: {
+//     type: db.Sequelize.STRING,
+//     allowNull: false,
+//     // unique: true, // pra manter o email unico e não acontecer de registrar o mesmo email no banco
+//   },
+// });
+
+// Usuario.sync({ alter: true }); // alter: true juntamente com unique: true - Verifica diferenças
+
+// module.exports = Usuario;
